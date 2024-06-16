473e4ed1fd9d348a3e6f488a0f78f195b0254624
Yann Reis <160686328+YannReis@users.noreply.github.com>
2024-06-16 19:56:56 -0300

    Atualizar o Bebida.java
    javadoc

--- src/Bebida.java
+++ src/Bebida.java
@@ -1,38 +1,72 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe Bebida representa uma bebida com seu nome, preço, unidade e fabricante.
+ */
 public class Bebida {
     private String nome;
     private double preco;
     private String unidade;
     private String fabricante;
 
-    public Bebida(String nome, double preco, String unidade, String fabricante) {               //Cria o construtor da classe bebidas 
-        this.nome = nome;                                                                     //Inicia atributo
-        this.preco = preco;                                                                 //Inicia atributo
-        this.unidade = unidade;                                                            //Inicia atributo
-        this.fabricante = fabricante;                                                     //Inicia atributo
+    /**
+     * Construtor da classe Bebida.
+     *
+     * @param nome       O nome da bebida.
+     * @param preco      O preço da bebida.
+     * @param unidade    A unidade da bebida (por exemplo, ml, lata, garrafa, etc.).
+     * @param fabricante O fabricante da bebida.
+     */
+    public Bebida(String nome, double preco, String unidade, String fabricante) {
+        this.nome = nome;
+        this.preco = preco;
+        this.unidade = unidade;
+        this.fabricante = fabricante;
     }
-    //Define metodos get publicos para acessar os atributos privados das classes 
 
-    public String getNome() {
+    /**
+     * Retorna o nome da bebida.
+     *
+     * @return O nome da bebida.
+     */
+    public String retornaNome() {
         return nome;
     }
 
-    public double getPreco() {
+    /**
+     * Retorna o preço da bebida.
+     *
+     * @return O preço da bebida.
+     */
+    public double retornaPreco() {
         return preco;
     }
 
-    public String getUnidade() {
+    /**
+     * Retorna a unidade da bebida.
+     *
+     * @return A unidade da bebida.
+     */
+    public String retornaUnidade() {
         return unidade;
     }
 
-    public String getFabricante() {
+    /**
+     * Retorna o fabricante da bebida.
+     *
+     * @return O fabricante da bebida.
+     */
+    public String retornaFabricante() {
         return fabricante;
     }
 
-    //Define um método getBebidas estático que retorna uma lista de objetos Bebida
-    public static List<Bebida> getBebidas() {
+    /**
+     * Retorna uma lista de bebidas pré-definidas.
+     *
+     * @return Uma lista de objetos Bebida.
+     */
+    public static List<Bebida> retornaBebidas() {
         List<Bebida> bebidas = new ArrayList<>();
         bebidas.add(new Bebida("Coca-Cola lata", 5.00, "350ml", "Coca-Cola Company"));
         bebidas.add(new Bebida("Guaraná Antarctica lata", 4.50, "350ml", "Ambev"));

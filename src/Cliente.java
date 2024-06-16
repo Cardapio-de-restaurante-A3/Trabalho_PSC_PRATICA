ae7498d5a687ea3f1c4e2bdfceae54ea098b40f8
Yann Reis <160686328+YannReis@users.noreply.github.com>
2024-06-16 19:58:12 -0300

    Atualizar o Cliente.java
    javadoc

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,29 +2,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * A classe Cliente representa um cliente com seu nome, idade e email.
+ */
 public class Cliente {
     private String nome;
     private int idade;
     private String email;
 
-    public Cliente(String nome, int idade, String email) {            //Define o construtor da classe e declara 
+    /**
+     * Construtor da classe Cliente.
+     *
+     * @param nome  O nome do cliente.
+     * @param idade A idade do cliente.
+     * @param email O email do cliente.
+     */
+    public Cliente(String nome, int idade, String email) {
         this.nome = nome;
         this.idade = idade;
         this.email = email;
     }
 
+    /**
+     * Retorna o nome do cliente.
+     *
+     * @return O nome do cliente.
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Retorna a idade do cliente.
+     *
+     * @return A idade do cliente.
+     */
     public int getIdade() {
         return idade;
     }
 
+    /**
+     * Retorna o email do cliente.
+     *
+     * @return O email do cliente.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Salva o total de clientes em um arquivo CSV.
+     *
+     * @param clientes A lista de clientes.
+     */
     public static void salvarTotalClientesCSV(List<Cliente> clientes) {
         try (FileWriter writer = new FileWriter("total_clientes.csv")) {
             writer.append("Total de Clientes\n");
